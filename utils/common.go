f2047897f4c5a69416fad803a1ad2cfdafc44f3b
yubc <yubc2006@sohu.com>
2018-02-08 18:11:48 +0800

    增加页面

--- utils/common.go
+++ utils/common.go
@@ -12,7 +12,7 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 func IsExistString(str string, strList []string) bool {
 	l := len(strList)
 	for i := 0; i < l; i++ {
-		if strings.Contains(strList[i], str) {
+		if strings.Contains(str, strList[i]) {
 			return true
 		}
 	}
